7131a4fb6add3398902c3f63b6e561a622554647
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2024-12-12 16:28:30 +0100

    add basic navigation

--- composeApp/src/commonMain/kotlin/com/jpmobilelab/kmp/weatherapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/jpmobilelab/kmp/weatherapp/App.kt
@@ -2,6 +2,10 @@ package com.jpmobilelab.kmp.weatherapp
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
 import com.jpmobilelab.kmp.weatherapp.weather.ui.WeatherScreenRoot
 import com.jpmobilelab.kmp.weatherapp.weather.ui.WeatherViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -11,9 +15,20 @@ import org.koin.compose.viewmodel.koinViewModel
 @Preview
 fun App() {
     MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = Route.WeatherGraph
+        ) {
+            navigation<Route.WeatherGraph>(
+                startDestination = Route.WeatherHome
+            ) {
+                composable<Route.WeatherHome> {
+                    val viewModel = koinViewModel<WeatherViewModel>()
 
-        val viewModel = koinViewModel<WeatherViewModel>()
-
-        WeatherScreenRoot(viewModel)
+                    WeatherScreenRoot(viewModel)
+                }
+            }
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/jpmobilelab/kmp/weatherapp/Route.kt
@@ -0,0 +1,12 @@
+package com.jpmobilelab.kmp.weatherapp
+
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+
+    @Serializable
+    data object WeatherGraph : Route
+
+    @Serializable
+    data object WeatherHome : Route
+}
