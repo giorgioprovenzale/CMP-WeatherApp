da63d3a3bd5fd209921b93a2979efbfa9e178d00
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2024-12-14 17:10:19 +0100

    remove Preview

--- composeApp/src/androidMain/kotlin/com/jpmobilelab/kmp/weatherapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jpmobilelab/kmp/weatherapp/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +24,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
